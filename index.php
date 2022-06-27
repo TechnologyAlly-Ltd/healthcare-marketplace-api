06d1e2c677ad4562f6c335c236ebd3adcb71bf2d
Shaiq Ul Hassan <shaiq.hassan@technologyally.com>
2022-06-27 17:59:46 +0500

    Testing changes

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ var_dump($healthcare->Plans()->searchInsurancePlansStats([
       "state": "VA",
       "zipcode": "20103"
     },
-    "market": "Any",
+    "market": "Individual",
     "filter": {
       "disease_mgmt_programs": [
         "Pain Management",
@@ -39,7 +39,7 @@ var_dump($healthcare->Plans()->searchInsurancePlansStats([
       ],
       "deductible": 55240280.28049317,
       "hsa": false,
-      "oopc": "-13470224.042113096",
+      "oopc": 1,
       "child_dental_coverage": false,
       "adult_dental_coverage": true,
       "drugs": [
@@ -50,16 +50,8 @@ var_dump($healthcare->Plans()->searchInsurancePlansStats([
         "2517064711",
         "7055410232"
       ],
-      "quality_rating": "-60162000.35155159",
-      "simple_choice": false,
-      "premium_range": {
-        "min": "-33113986.03334319",
-        "max": "-39176424.04392558"
-      },
-      "deductible_range": {
-        "min": "-50994448.00503386",
-        "max": "-62684214.70006933"
-      }
+      "quality_rating": 3,
+      "simple_choice": false
     },
     "household": {
       "income": 16106745.324259907,
@@ -67,53 +59,49 @@ var_dump($healthcare->Plans()->searchInsurancePlansStats([
       "people": [
         {
           "age": 24,
-          "dob": "12-12-2020",
+          "dob": "2006-01-02",
           "has_mec": false,
           "is_parent": true,
           "is_pregnant": false,
-          "pregnant_with": 91950208.02053279,
+          "pregnant_with": 1,
           "uses_tobacco": false,
-          "last_tobacco_use_date": "12-12-2020",
+          "last_tobacco_use_date": "2006-01-02",
           "gender": "Male",
           "utilization_level": "Medium",
-          "relationship": {},
+          "relationship": "spouse",
           "does_not_cohabitate": false,
           "aptc_eligible": false,
           "current_enrollment": {
-            "plan_id": {
-              "value": "<Error: Too many levels of nesting to fake this schema>"
-            },
-            "effective_date": "12-12-2020",
+            "plan_id": "11512NC0100031",
+            "effective_date": "2006-01-02",
             "uses_tobacco": false
           }
         },
         {
-          "age": "-55756964.083645366",
-          "dob": "12-12-2020",
+          "age": 24,
+          "dob": "2006-01-02",
           "has_mec": false,
           "is_parent": false,
           "is_pregnant": false,
-          "pregnant_with": 79854236.14225131,
+          "pregnant_with": 1,
           "uses_tobacco": false,
-          "last_tobacco_use_date": "12-12-2020",
+          "last_tobacco_use_date": "2006-01-02",
           "gender": "Male",
           "utilization_level": "High",
-          "relationship": {},
+          "relationship": "spouse",
           "does_not_cohabitate": true,
           "aptc_eligible": true,
           "current_enrollment": {
-            "plan_id": {
-              "value": "<Error: Too many levels of nesting to fake this schema>"
-            },
-            "effective_date": "12-12-2020",
+            "plan_id": "11512NC0100031",
+            "effective_date": "2006-01-02",
             "uses_tobacco": true
           }
         }
       ],
       "has_married_couple": false,
-      "effective_date": "12-12-2020"
+      "effective_date": "2006-01-02"
     },
-    "offset": "-63457035.80180224",
+    "offset": 0,
     "order": "asc",
     "sort": "quality_rating",
     "year": 2019,
@@ -121,7 +109,5 @@ var_dump($healthcare->Plans()->searchInsurancePlansStats([
     "csr_override": "LimitedCSR",
     "catastrophic_override": true,
     "suppressed_plan_ids": [
-      "16039GJ5803639,06987IR9096639,16578TE7296943",
-      "57878JA2611362,96871QR3702956,63900GZ8582612,42378DY7819486,55415NT5262332,14254CM1989156"
     ]
   }', true)));
